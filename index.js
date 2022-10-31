b9f64a479c9c21ac240424fd7010d330598dd3a4
haunv <haunv@gmail.com>
2022-10-31 22:29:00 +0700

    hide router and test heroku

--- index.js
+++ index.js
@@ -23,5 +23,6 @@ app.use(express.json())
 // app.use('/api/posts', postRouter)
 
 
-const PORT = 5001;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
